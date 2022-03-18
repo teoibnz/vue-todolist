a65dd3ba4678076bb0ca402d454cb5cd52f33f1a
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-18 16:25:07 +0100

    inizializzazione file e creazione ayout nel dom

---
+++ main.js
@@ -0,0 +1,34 @@
+const app = new Vue (
+    {
+        el : '#app',
+        
+        data : {
+            images : [
+                {
+                    nome : 'First Guitar',
+                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-1.jpeg', 
+                },
+                {
+                    nome : 'Second Guitar',
+                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-2.jpeg',
+                },
+                {
+                    nome : 'Third Guitar',
+                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-3.jpeg',
+                },
+                {
+                    nome : 'Fourth Guitar',
+                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-strato.jpeg',
+                },
+            ],
+        },
+        
+        methods : {
+
+            
+
+        },
+        
+    } 
+);
+
