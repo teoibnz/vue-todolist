eac6b27c89feadaca3ed1cbf986c82be40ce5ea0
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-18 18:23:44 +0100

    aggiunta dei todo item

--- main.js
+++ main.js
@@ -3,32 +3,40 @@ const app = new Vue (
         el : '#app',
         
         data : {
-            images : [
-                {
-                    nome : 'First Guitar',
-                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-1.jpeg', 
-                },
-                {
-                    nome : 'Second Guitar',
-                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-2.jpeg',
-                },
-                {
-                    nome : 'Third Guitar',
-                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-3.jpeg',
-                },
-                {
-                    nome : 'Fourth Guitar',
-                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-strato.jpeg',
-                },
-            ],
+            todoList : [
+                
+            ],  
+        newTodo : ''
         },
         
         methods : {
+            addNewTask(toDoItem){
+                if(toDoItem.trim() != ''){
+                    this.todoList.push(toDoItem)
+                    console.log(this.todoList)
+                } else {
+                    console.warn('inserisci almeno un valore!')
+                }
 
-            
+            },
+            removeTask(toDoIndex){
+                this.todoList.splice(toDoIndex , 1);
+            }
 
         },
         
-    } 
+} 
 );
+
+
+// Descrizione:
+// Rifare l'esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// text, una stringa che indica il testo del todo
+// done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
